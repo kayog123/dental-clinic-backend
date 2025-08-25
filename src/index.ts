4b469643954f3581631462fad0c9f90af72ff060
Jovanni Jungco <jovannijungco27@gmail.com>
2025-08-25 22:01:16 +0800

    update crossOriginPolicy

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "same-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
